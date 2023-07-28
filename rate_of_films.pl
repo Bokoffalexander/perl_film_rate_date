a480c26928e2ec68e6fd00596941d3c18754d8bc
Bokoffalexander <89662116+Bokoffalexander@users.noreply.github.com>
2023-07-28 15:19:48 +0300

    Create rate_of_films.pl

---
+++ rate_of_films.pl
@@ -0,0 +1,10 @@
+#!/usr/bin/perl
+# function-subroutine
+use 5.30.0;
+use strict;
+use warnings;
+##########
+#my $url = 'https://perlmaven.com';
+my $url = 'https://www.timeout.com/film/best-movies-of-all-time';
+system "wget $url";
+##########
